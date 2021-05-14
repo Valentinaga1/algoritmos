fcb30d5a9882fb65e746d89da726ee3f9edf06ed
Valentinaga1 <1881@holbertonschool.com>
2021-05-13 22:49:38 -0500

    Fixing pep

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+
 """ 
 method that calculates the fewest number of operations needed to result
 in exactly n H characters in the file.
 """
+
 
 def minOperations(n):
     """
