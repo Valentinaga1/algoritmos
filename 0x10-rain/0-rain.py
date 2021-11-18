ceea8f9cb4b407dd7dd2e4be606eced574f05b01
Valentinaga1 <valentinagoa2@gmail.com>
2021-11-18 10:50:20 -0500

    Fixing error

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -3,42 +3,16 @@
 """
 
 
-def rain(walls) -> int:
-    """ rain calculates
-    Args:
-        walls (list): non-negative integers representing
-        the heights of walls with unit width 1
-    Returns:
-        [int]: represent the sum of all water retained
-    """
-    if not isinstance(walls, list):
-        return 0
-
-    if walls == [None] or walls == []:
-        return 0
-
-    set_walls = set(walls)
-    if set_walls == {0, 1} or set_walls == {1} \
-       or set_walls == {0}:
-        return 0
-
-    list1 = []
-    list2 = []
-    for i, v in enumerate(walls, 1):
-        if v != 0:
-            list1.append(i)
-            list2.append(v)
-
-    add = 0
-    for j in range(len(list1)):
-        i1 = list1[j]
-        if j < len(list1) - 1:
-            i2 = list1[j+1]
-        index = i2 - i1 - 1
-        v1 = list2[j]
-        if j < len(list1) - 1:
-            v2 = list2[j + 1]
-        value = v1 if v1 < v2 else v2
-        add += index * value
-        if j+1 == len(list1) - 1:
-            return add
+def rain(walls):
+    """ check how many square units of
+    water will be retained after it rains """
+    sum = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        sum += (min(left, right) - walls[i])
+    return sum
